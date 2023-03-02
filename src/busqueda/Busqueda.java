c088f8890d806aa334a604a210b0c3c6b975c81b
HP PORTATIL <HP PORTATIL@HPPORTATIL>
2023-03-01 22:39:35 -0500

    Fix Funcion generarRectaNumerica Fix Funcion generateRandomArrayList Create metodo Buscar

--- src/busqueda/Busqueda.java
+++ src/busqueda/Busqueda.java
@@ -40,6 +40,34 @@ public class Busqueda {
         return tiempo;
     }
     
+    public static int Buscar(int Iinicial, int BNum, ArrayList<Integer> lista){
+        int posicion = Iinicial;
+        int centro = lista.size()/2;
+        
+        if(lista.size()<=1){
+            posicion = -1;
+        }else if(lista.get(centro)== BNum){
+            posicion = posicion + centro;
+        }else{
+            ArrayList<Integer> sublista = new ArrayList<>();
+            int inicio;
+            if (BNum < lista.get(centro)){
+                inicio = Iinicial;
+                for(int i = inicio ; i<centro ; i++){
+                    sublista.add(lista.get(i));
+                }
+            }else{
+                inicio = Iinicial + centro+1;
+                for(int i = inicio ; i<lista.size() ; i++){
+                    sublista.add(lista.get(i));
+                }
+            }
+            posicion=Buscar(inicio, BNum, sublista);
+        }
+        
+        return posicion;
+    }
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Ingresa el tipo del arreglo\n- ");
@@ -49,7 +77,12 @@ public class Busqueda {
         
         long TiempoGeneracion = Generar(Tipo, Tamaño);
         
-        System.out.println("Arreglo: "+Lista+"\nTiempo de generacion"+TiempoGeneracion);
+        System.out.println("Arreglo: "+Lista+"\nTiempo de generacion: "+TiempoGeneracion);
+        
+        System.out.print("\nIngrese el numero que busca\n- ");
+        int busqueda = in.nextInt();
+        
+        System.out.println("El indise es "+Buscar(0, busqueda, Lista));
     }
     
 }
