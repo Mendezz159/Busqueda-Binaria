1b5ad9093af2a9ec6df3afa9c4af663c92ba2528
HP PORTATIL <HP PORTATIL@HPPORTATIL>
2023-03-01 21:10:32 -0500

    Funciones de generado añadidas Funcion Quick sort añadida Funcion Generar Completada

--- src/busqueda/Busqueda.java
+++ src/busqueda/Busqueda.java
@@ -4,17 +4,43 @@
  */
 package busqueda;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author HP PORTATIL
  */
 public class Busqueda {
 
+    private static ArrayList<Integer> Lista = new ArrayList<>();
+    
     /**
-     * @param args the command line arguments
+     * Genera el arraylist dependiendo de una eleccion
+     * @param Tipo 1. Recta Numerica 2. Aleatoreo
+     * @param Tamaño El tamaño del arraylis
+     * @return tiempo que se tarda generando en milisegundos
      */
+    public static long Generar(int Tipo, int Tamaño){
+        long tiempo = System.currentTimeMillis();
+        
+        switch(Tipo){
+            case 1:
+                Lista=Metodos.generarRectaNumerica(Tamaño);
+                break;
+            case 2:
+                Lista = Metodos.generateRandomArrayList(Tamaño);
+                Lista = Metodos.Quicksort(Lista);
+                break;
+        }
+        
+        tiempo = System.currentTimeMillis() - tiempo;
+        
+        return tiempo;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
---
+++ src/busqueda/Metodos.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package busqueda;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author HP PORTATIL
+ */
+public class Metodos {
+    
+    /**
+     * Retorna un arraylist con numero aleatoreos
+     * @param size Tamaño del array
+     * @return El array generado
+    **/
+    public static ArrayList<Integer> generateRandomArrayList(int size) {
+        ArrayList<Integer> arrayList = new ArrayList<>(size);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            arrayList.add(random.nextInt());
+        }
+        return arrayList;
+    }
+    
+    /**
+     * Retorna un arraylist con una recta numerica
+     * @param tamano Tamaño del array
+     * @return El array generado
+    **/
+    public static ArrayList<Integer> generarRectaNumerica(int tamano) {
+        ArrayList<Integer> arrayList = new ArrayList<>(tamano);
+        for (int i = 0; i < tamano; i++) {
+            arrayList.add(i);
+        }
+        return arrayList;
+    }
+    
+    /**
+     * Retorna un arraylist Organizado
+     * @param Lista El arraylis desordenado
+     * @return El arraylis Ordenado
+    **/
+    public static ArrayList<Integer> Quicksort(ArrayList<Integer> Lista){
+        if (Lista.size() <= 1){
+            return Lista;
+        }
+        
+        int pivotIndex = Lista.size()/2;
+        int pivotValue = Lista.get(pivotIndex);
+        
+        ArrayList<Integer> menores = new ArrayList<>();
+        ArrayList<Integer> mayores = new ArrayList<>();
+        ArrayList<Integer> iguales = new ArrayList<>();
+        
+        for (int i = 0; i < Lista.size(); i++) {
+            int value = Lista.get(i);
+            if (value < pivotValue) {
+                menores.add(value);
+            } else if (value > pivotValue) {
+                mayores.add(value);
+            } else {
+                iguales.add(value);
+            }
+        }
+        
+        ArrayList<Integer> ordenado = new ArrayList<>();
+        ordenado.addAll(Quicksort(menores));
+        ordenado.addAll(iguales);
+        ordenado.addAll(Quicksort(mayores));
+        
+        return ordenado;
+    }
+}
